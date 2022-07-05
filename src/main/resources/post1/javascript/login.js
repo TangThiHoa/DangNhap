f58c34a6ea643e6312ea23351f3ac3b2dc4365c8
TangThiHoa <hoarua592001@gmail.com>
2022-07-05 16:47:35 +0700

    xong dang ky va thu alerts modal

---
+++ src/main/resources/post1/javascript/login.js
@@ -0,0 +1,90 @@
+document.getElementById('content').innerHTML = `
+<div class="main">
+            <input type="checkbox" id="chk" aria-hidden="true">
+
+            <div class="signup" id="su">
+                <div>
+                    <label for="chk" aria-hidden="true">Sign up</label>
+            <input type="text" name="username" id="username" placeholder="User name" required="">
+            <input type="text" name="password" id="password" placeholder="Password" required="">
+            <input type="text" name="confirmPassword" id="confirmPassword" placeholder="Confirm Password" required="">
+            <button onclick="singUp()">Sign up</button>
+        </div>
+    </div>
+
+    <div class="login" id="ln">
+        <div>
+            <label for="chk" aria-hidden="true">Login</label>
+            <input type="username" name="username" placeholder="User name" required="">
+            <input type="password" name="password" placeholder="Password" required="">
+            <button onclick="login()">Login</button>
+        </div>
+    </div>
+</div>
+`
+
+let id;
+let token;
+let storageKey = 'token';
+let storageKeyId = 'id';
+
+function login() {
+    let userName = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
+    let user = {
+        username: userName,
+        password: password,
+    }
+    $.ajax({
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+        },
+        type: "POST",
+        url: "http://localhost:8000/login",
+        data: JSON.stringify(user),
+        success: function (data) {
+            console.log(data)
+            token = data.accessToken;
+            id = data.id;
+            localStorage.setItem(storageKey, token)
+            localStorage.setItem(storageKeyId, id)
+            // main();
+        },
+        error: function (error) {
+            console.log(error)
+        }
+    })
+}
+
+
+function singUp() {
+    let userName = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
+    let confirmPassword = document.getElementById("confirmPassword").value;
+    let user = {
+        username: userName,
+        password: password,
+        confirmPassword: confirmPassword
+    }
+    $.ajax({
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+        },
+        type: "POST",
+        url: "http://localhost:8000/register",
+        data: JSON.stringify(user),
+        success: function () {
+            $('#username').val("");
+            $('#password').val("");
+            $('#confirmPassword').val("");
+            alert("Đăng kí thành công!")
+            singUp()
+        },
+        error: function (error) {
+            console.log(error)
+        }
+    })
+}
+
